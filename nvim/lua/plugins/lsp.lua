9e9eac280b38753169e3de3346259cf09bca5d64
Muhammad Inam Ul Haq <muhammadinamulhaq@hotmail.com>
2025-01-20 22:43:20 +0100

    disabled treestitter for vscode

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -135,40 +135,19 @@ return {
 				["ruff"] = function()
 					require("lspconfig").ruff.setup({
 						capabilities = capabilities,
-						settings = {
-							-- commands = {
-							-- 	RuffAutofix = {
-							-- 		function()
-							-- 			vim.lsp.buf.execute_command({
-							-- 				command = "ruff.applyAutofix",
-							-- 				arguments = {
-							-- 					{ uri = vim.uri_from_bufnr(0) },
-							-- 				},
-							-- 			})
-							-- 		end,
-							-- 		description = "Ruff: Fix all auto-fixable problems",
-							-- 	},
-							-- 	RuffOrganizeImports = {
-							-- 		function()
-							-- 			vim.lsp.buf.execute_command({
-							-- 				command = "ruff.applyOrganizeImports",
-							-- 				arguments = {
-							-- 					{ uri = vim.uri_from_bufnr(0) },
-							-- 				},
-							-- 			})
-							-- 		end,
-							-- 		description = "Ruff: Format imports",
-							-- 	},
-							-- },
-						},
+						settings = {},
 					})
 				end,
 
 				["clangd"] = function()
+					print("Attaching clangd")
 					lspconfig.clangd.setup({
 						settings = {},
 						capabilities = capabilities,
-						cmd = {},
+						filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+						cmd = {
+							"clangd",
+						},
 					})
 				end,
 
