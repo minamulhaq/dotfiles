e8cdcc1e1b7f3f8fddbff7efa6f33bfca3cf753f
Muhammad Inam Ul Haq <muhammadinamulhaq@hotmail.com>
2025-09-21 02:06:48 +0200

    added tmux settigns

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,7 +1,38 @@
-vim.o.background = "dark" -- ensure dark mode
 local theme = "gruvbox"
--- local theme = "rose-pine"
-if theme == "gruvbox" then
-    vim.g.gruvbox_contrast_dark = "hard"
+-- Default options:
+require("gruvbox").setup({
+    terminal_colors = true, -- add neovim terminal colors
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    inverse = true,    -- invert background for search, diffs, statuslines and errors
+    contrast = "hard", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = true,
+})
+vim.cmd("colorscheme gruvbox")
+
+
+function ColorMyPencils(color)
+    vim.o.background = "dark"
+    color = color or "gruvbox"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
-vim.cmd.colorscheme(theme)
+
+ColorMyPencils(theme)
