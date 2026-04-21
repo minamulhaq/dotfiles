9d00849abed7ece68f7e166afb5f3ae40dcd10ce
Muhammad Inam Ul Haq <muhammadinamulhaq@hotmail.com>
2026-04-21 20:45:33 +0200

    removed harpoon and treesitter from vscode

--- vscode-nvim/lua/Muhammad/plugins/init.lua
+++ vscode-nvim/lua/Muhammad/plugins/init.lua
@@ -1,6 +1,4 @@
 return {
-    require("Muhammad.plugins.treesitter"),
     require("Muhammad.plugins.header-guard"),
-    require("Muhammad.plugins.harpoon"),
     require("Muhammad.plugins.mini"),
 }
